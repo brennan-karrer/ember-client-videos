37c61f5d1cdfeaa002e91d659011792862c779e0
Brennan Karrer <Brennan.Karrer@Evisions.com>
2017-04-27 19:09:25 -0700

    Add adapter, serializer, route, model and template for videos

--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,9 @@ module.exports = function(environment) {
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
+    contentSecurityPolicy: {
+      'connect-src': "'self' http://localhost:3000"
+    },
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -14,7 +17,7 @@ module.exports = function(environment) {
       EXTEND_PROTOTYPES: {
         // Prevent Ember Data from overriding Date.parse.
         Date: false
-      }
+      },
     },
 
     APP: {
